d386a4a586139b13122d53902c65614452325a80
devops201801 <kp560@mail4-us.org>
2018-02-23 13:58:29 +0530

    added thirsty to it

--- first.py
+++ first.py
@@ -3,6 +3,7 @@ hungry = input("are you hungry")
 if hungry == "yes" and hungry=="y":
     print("eat samosa")
     print("eat pizza")
-    print("eat pasta")
 else:
-    print("do your homework")
+    thirsty=input("are you thirsty")
+    if thirsty=="yes":
+        print("drink water")
