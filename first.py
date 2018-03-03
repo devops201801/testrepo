a52d44322b9569dafcc0f2aece2f585d24efcc03
ramubuntu <ram@ubuntu.com>
2018-03-04 02:13:03 +0530

    commit from ubuntu

--- first.py
+++ first.py
@@ -5,6 +5,6 @@ if hungry == "yes" and hungry=="y":
     print("eat pizza")
 else:
     thirsty=input("are you thirsty")
-    if thirsty=="yes" and thirsty=="y":
+    if thirsty=="yes":
         print("drink water")
         print("drink soda")
